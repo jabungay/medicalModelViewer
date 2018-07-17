bf5b9e788ee3680eba669c91faa08e14183e6543
Jonathan Bungay <jabungay@mun.ca>
2018-07-17 15:02:10 -0230

    Modified Styling

--- account.php
+++ account.php
@@ -1,2 +1,19 @@
 <?php
-echo "future location of account settings" ?>
+echo "future location of account settings"
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Account Settings</title>
+    <link rel="stylesheet" href="/style.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+  <button onclick="window.location.href='logout.php'"> Log Out </button>
+</body>
